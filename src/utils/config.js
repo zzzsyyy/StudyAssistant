a9481357d28ad9f49214211d5a2fdff5e1afa8fa
zzzsyyy <me@zzzsy.top>
2022-04-03 21:30:18 +0800

    update: complete loading component update: fix: 'substring not found' in GQ

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-const version = "0.65_beta";
+const version = "0.68_beta";
 const contact = {
   email: "me@zzzsy.top",
   wx_id: "zsyyy_8",
@@ -20,8 +20,14 @@ const pages = {
   questions: "/pages/entryDetail/questions"
 };
 const errors = {
-  no_entry: "Entry matching query does not exist."
+  no_entry: "Entry matching query does not exist.",
+  no_questions: "substring not found"
 }
 
+// const apis = {
+//   entry : {
+
+//   }
+// }
 
 export { pages, version, contact, errors };
